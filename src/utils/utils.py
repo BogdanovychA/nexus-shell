ee24c720330e8fd0516103c01e593f05a98285de
Andrii Bogdanovych <a@bogdanovych.org>
2026-03-26 14:09:51 +0200

    Додано функцію відправлення повідомлення

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -15,6 +15,10 @@ if TYPE_CHECKING:
     from aiogram import Bot
 
 
+async def send_message(bot: Bot, chat_id: int, text: str):
+    await bot.send_message(chat_id, text)
+
+
 async def save_file(message: Message, bot: Bot, filetype: FileType, base_path: Path):
     extensions = {
         FileType.VIDEO: "mp4",
